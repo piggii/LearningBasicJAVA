de5fcd9f14cd0a2ca0977da423db452829203ebd
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-04 18:43:08 +0700

    :pencil2: Add for loop

--- BasicMethod_getTotal.java
+++ BasicMethod_getTotal.java
@@ -8,12 +8,24 @@ public class BasicMethod_getTotal {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter 1st number : ");
-        int num1 = input.nextInt();
-        System.out.print("Enter 2st number : ");
-        int num2 = input.nextInt();
-        int sum = getTotal(num1, num2);
-        System.out.print("Sum = " + sum);
-        input.close();
+        Scanner inputChar = new Scanner(System.in);
+        for (;;) {
+            System.out.print("Enter 1st number : ");
+            int num1 = input.nextInt();
+            System.out.print("Enter 2st number : ");
+            int num2 = input.nextInt();
+            int sum = getTotal(num1, num2);
+            System.out.println("Sum = " + sum);
+            System.out.println("Do you want to exit the program? [y/n]");
+            char Quit = inputChar.nextLine().charAt(0);
+            int exit = (int) Quit;
+            if (exit == 89 || exit == 121) {
+                System.out.print("Exit program");
+                break;
+            } else {
+                continue;
+            }
+        }
+
     }
 }
