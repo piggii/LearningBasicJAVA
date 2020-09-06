383451df89e6ce86220d13a1ce6d500869467405
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-06 14:50:30 +0700

    :pencil2: Find minimum number

---
+++ BasicMethod_minNum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BasicMethod_minNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        float num1 = input.nextFloat();
+        System.out.print("Enter second number : ");
+        float num2 = input.nextFloat();
+        System.out.print("Enter third number : ");
+        float num3 = input.nextFloat();
+        System.out.print("The Smallest number is "+(minimum(num1,num2,num3)));
+
+    }
+    public static float minimum(float number1, float number2, float number3){
+        float number = Math.min(Math.min(number1,number2),number3);
+        return number;
+    }
+}
