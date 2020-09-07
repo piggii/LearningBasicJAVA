db4978460e8dd49b01bf77c33a347ef8ccf3ace3
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-07 22:02:47 +0700

    :tada: Add BasicLoop folder

---
+++ BasicLoop/BasicLoop_printNum.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
+public class BasicLoop_printNum{
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
