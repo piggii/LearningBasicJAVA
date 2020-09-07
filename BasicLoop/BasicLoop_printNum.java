2cc99b68b949187ece810aa2c9fc47926ed6c793
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-07 22:06:09 +0700

    :pencil: Basic for loop and delay

--- BasicLoop/BasicLoop_printNum.java
+++ BasicLoop/BasicLoop_printNum.java
@@ -1,8 +1,8 @@
 import java.util.concurrent.TimeUnit;
 
-public class BasicLoop_printNum{
+public class BasicLoop_printNum {
     public static void main(String[] args) throws InterruptedException {
-        for(int i = 1; i < 11; i++){
+        for (int i = 1; i < 11; i++) {
             System.out.println(i);
             TimeUnit.SECONDS.sleep(1);
         }
