5f0ac4bdb59cf5c3e22cbe1d52006ef79985896f
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-12 21:06:08 +0700

    :zap: Quiz 'Food ordering program'

--- Quiz/FoodOrder.java
+++ Quiz/FoodOrder.java
@@ -24,7 +24,7 @@ public class FoodOrder {
       if (price == 1) {
         sum = sum - 1;
         System.out.println("----------------------------------");
-        System.out.println("       Total : " + sum + " Baht       ");
+        System.out.println("       Total : " + sum + " Baht   ");
         System.out.println("----------------------------------");
         break;
       }
