950ef08c3c9de2922fc9cbbdc5847f361b5628db
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-15 14:58:33 +0700

    :lipstick: Print star

---
+++ BasicLoop/BasicLoop_Pattern.java
@@ -0,0 +1,12 @@
+
+public class BasicLoop_Pattern {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 6; i++){
+            for(int j = 1; j <= 1+i; j++){
+                System.out.print("*");
+                
+            }
+            System.out.println("");
+        }
+    }     
+}
