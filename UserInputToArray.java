ae783f868ec42e4e3873431b09bd801d5881c31d
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-24 22:43:59 +0700

    :rocket: Test user input for an Array

--- UserInputToArray.java
+++ UserInputToArray.java
@@ -3,24 +3,25 @@ import java.util.*;
 public class UserInputToArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int size = 0;
+        
         String exit = "EXIT";
         System.out.println("Enter new menu : ");
+        int size = 0;
+        String myMenu[] = new String[size];
         for(;;){
-            String myMenu[] = new String[size];
             String Menu = input.nextLine();
             int newSize = myMenu.length + 1;
             String newMenu[] = new String[newSize];
             System.arraycopy(myMenu,0,newMenu,0,myMenu.length);
             myMenu = newMenu;
-            myMenu[myMenu.length -1 ] = Menu;
+            myMenu[myMenu.length -1] = Menu;
             if(Menu.toUpperCase().equals(exit)){
+                for(int i = 0; i < myMenu.length; i++){
+                    System.out.println(i+1+"."+myMenu[i]);
+                }  
                 break;
             }
-            for(int i = 0; i < myMenu.length; i++){
-                System.out.println(myMenu[i]);
-            }
-           
+            
         }
     }
 }
