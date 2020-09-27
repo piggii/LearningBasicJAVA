bca3a70f8fd5b02273dad1b25d425ee382c4a3a4
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-27 23:33:39 +0700

    :rocket: Try to use ArrayList

--- Quiz/arrayList.java
+++ Quiz/arrayList.java
@@ -10,12 +10,12 @@ public class arrayList {
         for (;;) {
             System.out.print("Enter new menu : ");
             String Menu = inputMenu.nextLine();
-            if (Menu.toUpperCase().equals(exit)) { 
+            if (Menu.toUpperCase().equals(exit)) {
                 System.out.println("----------------------------------");
                 System.out.println("               MENU               ");
                 System.out.println("----------------------------------");
                 for (int i = 0; i < myMenu.size(); i++) {
-                    System.out.println(i + 1 + ". " + myMenu.get(i) + myPrice.get(i)+" Baht");
+                    System.out.println(i + 1 + ". " + myMenu.get(i) + myPrice.get(i) + " Baht");
                 }
                 System.out.println("----------------------------------");
                 break;
@@ -27,9 +27,10 @@ public class arrayList {
             myPrice.add(price);
         }
     }
+
     public static String spaceBar(int i) {
-        String space ="";
-        for(int j = i ; j < 24; j++){
+        String space = "";
+        for (int j = i; j < 24; j++) {
             String newSpace = " ";
             space = space + newSpace;
         }
