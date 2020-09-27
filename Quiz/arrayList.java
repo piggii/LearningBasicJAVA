717bde23083b776bca726a8eaa2638050b866135
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-27 23:30:52 +0700

    :rocket: Try to use ArrayList

--- Quiz/arrayList.java
+++ Quiz/arrayList.java
@@ -2,23 +2,37 @@ import java.util.*;
 
 public class arrayList {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner inputMenu = new Scanner(System.in);
+        Scanner inputPrice = new Scanner(System.in);
         String exit = "EXIT";
         ArrayList<String> myMenu = new ArrayList<String>();
-        System.out.println("Enter new menu :");
+        ArrayList<Integer> myPrice = new ArrayList<Integer>();
         for (;;) {
-            String Menu = input.next();
-            if (Menu.toUpperCase().equals(exit)) {
+            System.out.print("Enter new menu : ");
+            String Menu = inputMenu.nextLine();
+            if (Menu.toUpperCase().equals(exit)) { 
                 System.out.println("----------------------------------");
                 System.out.println("               MENU               ");
                 System.out.println("----------------------------------");
                 for (int i = 0; i < myMenu.size(); i++) {
-                    System.out.println(i + 1 + ". " + myMenu.get(i));
+                    System.out.println(i + 1 + ". " + myMenu.get(i) + myPrice.get(i)+" Baht");
                 }
                 System.out.println("----------------------------------");
                 break;
             }
+            Menu = Menu + spaceBar(Menu.length());
             myMenu.add(Menu);
+            System.out.print("Enter price : ");
+            int price = inputPrice.nextInt();
+            myPrice.add(price);
         }
+    }
+    public static String spaceBar(int i) {
+        String space ="";
+        for(int j = i ; j < 24; j++){
+            String newSpace = " ";
+            space = space + newSpace;
+        }
+        return space;
     }
 }
