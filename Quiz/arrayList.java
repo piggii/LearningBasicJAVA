beff41f8cf300ef7e58dd9655397f6d408b83724
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-09-27 21:58:13 +0700

    :rocket: Try to use ArrayList

--- Quiz/arrayList.java
+++ Quiz/arrayList.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 public class arrayList {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String exit = "EXIT";
         ArrayList<String> myMenu = new ArrayList<String>();
         System.out.println("Enter new menu :");
-        for(;;){
+        for (;;) {
             String Menu = input.next();
-            if(Menu.toUpperCase().equals(exit)){
+            if (Menu.toUpperCase().equals(exit)) {
                 System.out.println("----------------------------------");
                 System.out.println("               MENU               ");
                 System.out.println("----------------------------------");
-                for(int i = 0; i < myMenu.size(); i++){
+                for (int i = 0; i < myMenu.size(); i++) {
                     System.out.println(i + 1 + ". " + myMenu.get(i));
                 }
                 System.out.println("----------------------------------");
