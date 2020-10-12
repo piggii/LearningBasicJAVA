3bd68637b3fd25fb112b981c512aad07ab503bf9
TANAPORN-DEV <70440327+piggii@users.noreply.github.com>
2020-10-12 17:21:50 +0700

    Delete BasicHelloWorld.java

--- BasicHelloWorld.java
+++
@@ -1,7 +0,0 @@
-/*Basic Java Program to display "Hello World" */
-
-public class BasicHelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-    }
-}
