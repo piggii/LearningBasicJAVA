c2dbe459dc734acc5aedb482f2bd72ab8abd9853
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-10-17 16:30:28 +0700

    :mag: Test ToolBar

--- BasicGUI/src/main/java/BasicGUI/GUIToolBar.java
+++ BasicGUI/src/main/java/BasicGUI/GUIToolBar.java
@@ -8,19 +8,19 @@ public class GUIToolBar {
         JButton b1,b2,b3;
         JToggleButton b4,b5;
         
-        //Toolbar
+        // Toolbar
         JToolBar tb = new JToolBar();
         tb.setOrientation(SwingConstants.HORIZONTAL);
         tb.setName("Toolbar");
         
-        //Choose image to be used
+        // Choose image to be used
         ImageIcon open = new ImageIcon("C:\\Users\\Pictures\\folder.png");
         ImageIcon save = new ImageIcon("C:\\Users\\Pictures\\save-file.png");
         ImageIcon delete = new ImageIcon("C:\\Users\\Pictures\\delete.png");
         ImageIcon cut = new ImageIcon("C:\\Users\\Pictures\\cutting.png");
         ImageIcon copy = new ImageIcon("C:\\Users\\Pictures\\copy.png");
         
-        //Add image to button
+        // Add image to button
         b1 = new JButton(open);
         b1.setToolTipText("Open");
         b2 = new JButton(cut);
@@ -32,14 +32,14 @@ public class GUIToolBar {
         b5 = new JToggleButton(delete);
         b5.setToolTipText("Delete");
         
-        //Add button to the toolbar
+        // Add button to the toolbar
         tb.add(b1);
         tb.add(b2);
         tb.add(b3);
         tb.add(b4);
         tb.add(b5);
         
-        //Manage all frame
+        // Manage all frame
         JFrame window = new JFrame();
         window.setSize(400,300);
         window.setMaximizedBounds(new Rectangle(800,600));
