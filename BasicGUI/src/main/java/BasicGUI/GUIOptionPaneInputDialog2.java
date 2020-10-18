4bdb3b0c6b7addbb9eb25a4293530a36eb18f595
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-10-18 20:03:40 +0700

    :bento: Test OptionPane showInputDialog and Combobox

---
+++ BasicGUI/src/main/java/BasicGUI/GUIOptionPaneInputDialog2.java
@@ -0,0 +1,13 @@
+package BasicGUI;
+
+import javax.swing.*;
+
+public class GUIOptionPaneInputDialog2 {
+    public static void main(String[] args) {
+        String[] drinks = {"Coffee","Milk","Tea","Soda","Water","Cocktail"} ;
+        ImageIcon icon1 = new ImageIcon("C:\\Users\\Piggii\\Pictures\\drink.png");
+        Object select = JOptionPane.showInputDialog(null,"Choose a drink that you want","Drinks",
+                JOptionPane.PLAIN_MESSAGE,icon1,drinks,"Milk");
+        System.out.print("Your drink is : "+select);
+    }
+}
