6c568b6b48b3fec6e667db5507095f272ea745e3
TANAPORN-DEV <eyetanaporn.1105@gmail.com>
2020-10-19 13:13:58 +0700

    :construction: Test FlowLayout

---
+++ BasicGUI/src/main/java/BasicGUI/GUIComponentFlowLayout.java
@@ -0,0 +1,28 @@
+package BasicGUI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GUIComponentFlowLayout {
+    public static void main(String[] args) {
+        JButton button1 = new JButton("Open"); 
+        JButton button2 = new JButton("Close");
+        Cursor hc = new Cursor(Cursor.HAND_CURSOR);
+        FlowLayout f = new FlowLayout();
+        f.setAlignment(FlowLayout.LEFT);
+        f.setHgap(20);
+        
+        JFrame window = new JFrame();
+        window.setSize(400,300);
+        window.setMaximizedBounds(new Rectangle(800,600));
+        window.setTitle("Test Layout");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(f);
+        window.add(button1);
+        button1.setCursor(hc);
+        window.add(button2);
+        button2.setCursor(hc);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
